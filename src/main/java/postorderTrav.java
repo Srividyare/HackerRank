65d20721095a34ef2b1346c47de6537c158cdb56
Srividyare <srividya.re@gmail.com>
2016-08-01 13:25:10 -0500

    Create postorderTrav.java

---
+++ src/main/java/postorderTrav.java
@@ -0,0 +1,18 @@
+/* PostOrder Traversal */
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+
+void postOrder(Node root) {
+    if(root == null) return;
+    
+    postOrder(root.left);
+    postOrder(root.right);
+    System.out.print(root.data + "\t");
+
+}
