d6aa47ddc89ffc465f9101b8d141b77a42513e84
Srividyare <srividya.re@gmail.com>
2016-08-01 13:19:23 -0500

    Create preorderTrav.java

---
+++ src/main/java/preorderTrav.java
@@ -0,0 +1,19 @@
+/* Preorder  traversal*/
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+
+void preOrder(Node root) {
+    if (root==null) return;
+    System.out.print(root.data + "\t");
+    preOrder(root.left);
+    preOrder(root.right);
+    
+    
+
+}
