d2ae47ca46577200a93b9b846ed471d79e1bc287
Srividyare <srividya.re@gmail.com>
2016-08-06 23:02:34 -0500

    Create BSTInsert.java

---
+++ src/main/java/BSTInsert.java
@@ -0,0 +1,31 @@
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node Insert(Node root,int value)
+    {
+       if (root == null){
+           root = new Node();
+           root.data = value;
+           root.left=null;
+           root.right=null;
+           return root;
+       }
+    
+        if(root.data > value){
+            root.left = Insert(root.left, value);
+            
+        }
+    
+        else if(root.data < value){ 
+           root.right = Insert(root.right, value);
+            
+        }
+       return root;
+    }
+
+
